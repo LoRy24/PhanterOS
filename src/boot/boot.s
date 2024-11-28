8a9cb0a05a566752edf2d806340233d59c94e06e
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2024-11-28 19:46:43 +0100

    Costruita la base per l'extended bootloader

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -14,13 +14,11 @@
 
 .section                .rodata
 
-
 #
 # Messaggio di benvenuto
 #
 welcome_message:
                         .asciz      "Benvenuto in PantherOS!\n\rBy Lorenzo Rocca\n\r"
-
 
 #
 # Sezione del codice
@@ -28,10 +26,16 @@ welcome_message:
 
 .section                .text
 
+# 
+# Esportazione funzioni globali
+#
+
+.global                 _boot
+
 #
 # Offset del codice (compilatore)
 #
-.org                    $0x7c00
+.org                    $0x7C00
 
 #
 # Salta oltre il filesystem (0x03 bytes)
@@ -44,6 +48,9 @@ _boot:
 # Definizione del FileSystem FAT 16
 #
 fat_16_table:
+                        #
+                        # Configurazione FAT
+                        #
                         .ascii      "PHANTOS "                  # Nome OEM (lo personalizzo perchè si)
                         .word       0x0200                      # Numero di bytes per settore (512)
                         .byte       0x01                        # Settori ogni cluster
@@ -57,6 +64,10 @@ fat_16_table:
                         .word       0x0002                      # Numero testine
                         .long       0x00000000                  # Settori nascosti
                         .long       0x00000800                  # Numero totale di settori (2048)
+
+                        #
+                        # Extended BOOT record
+                        #
                         .byte       0x00                        # Numero drive
                         .byte       0x00                        # Flag riservato
                         .byte       0x28                        # Firma
@@ -68,14 +79,29 @@ fat_16_table:
 # Entry point del boot
 #
 _boot_program:
+                        #
+                        # Scrittura messaggio di benvenuto
+                        #
                         pusha                                   # Carica tutti i registri nello stack
                         mov         $welcome_message, %si       # Carica l'indirizzo della stringa in %si
                         call        print_string                # Scrivi la stringa in console
                         popa                                    # Ripristina lo stato iniziale dei registri
 
-                        pusha                                   # Carica tutti i registri nello stack
-                        mov         $0x0A, %cx                  # Carica il numero $0x0A in %cx
-                        call        print_num                   # Visualizza il numero a schermo
-                        popa                                    # Ripristina lo stato iniziale dei registri
+                        call        print_newline               # Vai nuovamente a capo
 
-                        jmp         .                           # Resta all'indirizzo di memoria attuale
+                        #
+                        # Lettura Extended bootloader
+                        #
+                        pusha                                   # Carica tutti i registri nello stack
+                        xor         %bx, %bx                    # Azzera %bx per poi azzerare %es
+                        mov         %bx, %es                    # Azzera %es
+                        mov         $0x7E00, %bx                # Carica l'indirizzo dove caricare l'extended bootloader
+                        mov         $0x04, %al                  # Numero di settori da leggere (4)
+                        mov         $0x02, %cl                  # Numero del settore da cui partire per leggere
+                        call        read_sectors_c0             # Esegui la funzione
+                        popa                                    # Ripristina tutti i registri
+
+                        #
+                        # Avvio Extended bootloader
+                        #
+                        jmp         0x7E00                      # Salta all'indirizzo dove è stato caricato
---
+++ src/boot/extended/extboot.s
@@ -0,0 +1,44 @@
+#
+# PANTHER OS by Abstract Panthers
+#
+# Autore: Lorenzo Rocca
+# File: extboot.s
+#
+
+# Codice a 16 bits
+.code16
+
+#
+# Sezione delle costanti
+#
+
+.section                .rodata
+
+#
+# Messaggio di benvenuto
+#
+welcome_message:
+                        .asciz      "Bootloader Esteso caricato!\n\r"
+
+#
+# Sezione del bootloader
+#
+
+.section                .bootloader
+
+#
+# Definizione funzioni globali
+#
+
+.global                 _main
+
+#
+# Entry point per l'extended bootloader
+#
+_main:
+                        pusha                                   # Carica tutti i registri nello stack
+                        mov         $welcome_message, %si       # Carica l'indirizzo della stringa in %si
+                        call        print_string                # Scrivi la stringa in console
+                        popa                                    # Ripristina lo stato iniziale dei registri
+
+                        jmp         .
